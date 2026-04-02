e16a80fda05263feb05b8efc458c2aac469cde36
JoaoMaiaBGM <joaocarlosbgm@gmail.com>
2026-04-02 10:38:24 -0300

    Feat OG image and SEO metrics

--- src/pages/politicas_de_privacidade.js
+++ src/pages/politicas_de_privacidade.js
@@ -1,20 +1,15 @@
-import Head from "next/head";
-
 import { Header } from "components/Header";
+import { Seo } from "components/Seo";
 import { _PrivacyPolicy } from "components/PrivacyPolicy";
 
 export default function PrivacyPolicy() {
   return (
     <>
-      <Head>
-        <title>Mineiríssimo</title>
-        <meta
-          name="description"
-          content="Page of privacy policy of the Mineiríssimo brazilian cheese bread store"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicons/logo_nova.ico" />
-      </Head>
+      <Seo
+        title="Política de privacidade | Mineiríssimo"
+        description="Política de privacidade do site Mineiríssimo."
+        canonicalPath="/politicas_de_privacidade"
+      />
       <main>
         <Header dark={"active"} size={"large"} />
         <_PrivacyPolicy />
