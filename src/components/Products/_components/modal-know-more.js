f107968c9f6a387cb6aa706b112166a98e1500d8
JoaoMaia <joaocarlosbgm@gmail.com>
2026-05-19 12:39:40 -0300

    Feat responsive modal and correct image dimensions

--- src/components/Products/_components/modal-know-more.js
+++ src/components/Products/_components/modal-know-more.js
@@ -3,7 +3,7 @@ import { FaCheckCircle } from 'react-icons/fa';
 import { MdClose } from 'react-icons/md';
 
 const Divider = () => {
-  return <div className="w-full h-1 bg-accent"></div>;
+  return <div className="w-full h-px bg-accent"></div>;
 };
 
 const ingredients = [
@@ -77,24 +77,28 @@ export function ModalKnowMore({ dialogId, product }) {
             <el-dialog-panel className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
               <div className="bg-primary-dark px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="flex flex-col items-start justify-center">
-                  <div className="w-full flex flex-row items-start gap-6">
-                    <div id="product-image" className="w-72 h-full p-0.5 bg-white rounded-lg">
+                  <div className="w-full flex flex-col items-center justify-center gap-6">
+                    <h3 id={titleId} className="h3 text-white">
+                      {product.title}
+                    </h3>
+
+                    <Divider />
+
+                    <div
+                      id="product-image"
+                      className="w-full max-h-5/12 h-full p-0.5 bg-white rounded-lg"
+                    >
                       <Image
                         src={product.url}
                         alt={product.alt}
-                        width={100}
-                        height={100}
+                        width={500}
+                        height={500}
                         className="w-full h-full object-cover rounded-lg"
+                        loading="eager"
                       />
                     </div>
 
                     <div id="product-information" className="flex flex-col items-center gap-2">
-                      <h3 id={titleId} className="h3 text-white">
-                        {product.title}
-                      </h3>
-
-                      <Divider />
-
                       <div
                         id="product-information-content"
                         className="flex flex-col items-start justify-center gap-4 mt-2"
